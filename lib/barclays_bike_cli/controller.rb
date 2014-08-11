b217b1bd0eabf06fd001b0c9645b975cbeb78d28
Rob Murray <robmurray17@gmail.com>
2014-08-11 21:35:08 +0100

    Add some default values to get Ruby 2.0.0 going

--- lib/barclays_bike_cli/controller.rb
+++ lib/barclays_bike_cli/controller.rb
@@ -1,11 +1,11 @@
 module BarclaysBikeCli
   class Controller
-    def initialize(repository:, renderer: default_renderer)
+    def initialize(repository: nil, renderer: default_renderer)
       @repository = repository
       @renderer = renderer
     end
 
-    def find_by_id(id:)
+    def find_by_id(id: nil)
       results = repository.find_by_id(id.to_i)
       renderer.render(results)
 
@@ -13,7 +13,7 @@ module BarclaysBikeCli
       renderer.render_error(query: "find_by_id: #{id}", error: e)
     end
 
-    def where(params:)
+    def where(params: {})
       results = repository.find_by_name(params[:name])
       renderer.render(results)
 
