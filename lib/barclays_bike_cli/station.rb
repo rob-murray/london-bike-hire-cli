cf79e38e59354fe8a175b01c177fde491fddfa20
Rob Murray <robmurray17@gmail.com>
2014-08-12 21:41:42 +0100

    Add map link url to Station

--- lib/barclays_bike_cli/station.rb
+++ lib/barclays_bike_cli/station.rb
@@ -1,5 +1,10 @@
 require 'ostruct'
 
 module BarclaysBikeCli
-  class Station < OpenStruct; end
+  class Station < OpenStruct
+
+    def map_link
+      "https://www.google.co.uk/maps/preview/@#{lat},#{long},17z"
+    end
+  end
 end
---
+++ spec/station_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+RSpec.describe BarclaysBikeCli::Station do
+  let(:params) do
+   { id: 1, name: 'test-station-1', docks_total: 30, docks_free: 10, lat: 51.53005939, long: -0.120973687 }
+  end
+  subject { BarclaysBikeCli::Station.new(params) }
+
+  describe '#map_link' do
+    it 'returns a link with lat and long' do
+      expect(subject.map_link).to eq('https://www.google.co.uk/maps/preview/@51.53005939,-0.120973687,17z')
+    end
+  end
+end
