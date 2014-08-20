d099a05c9f5fcdc360ce5eabdace1b70148efbf2
Rob Murray <robmurray17@gmail.com>
2014-08-20 22:10:40 +0100

    Add StationAdapter to transform each station into triples of 2points and ids

--- lib/barclays_bike_cli.rb
+++ lib/barclays_bike_cli.rb
@@ -4,6 +4,7 @@ require_relative 'barclays_bike_cli/controller'
 require_relative 'barclays_bike_cli/feed_parser'
 require_relative 'barclays_bike_cli/query_response'
 require_relative 'barclays_bike_cli/station'
+require_relative 'barclays_bike_cli/station_adapter'
 require_relative 'barclays_bike_cli/station_repository'
 require_relative 'barclays_bike_cli/version'
 
---
+++ lib/barclays_bike_cli/station_adapter.rb
@@ -0,0 +1,19 @@
+module BarclaysBikeCli
+  class StationAdapter
+    def initialize(stations:)
+      @stations = stations
+    end
+
+    # Public: Produce array of station triples
+    # E.g. [51.50810309, -0.12602103, 2]
+    # As used by kdtree
+    #
+    def to_triples
+      stations.map { |station| [station.lat, station.long, station.id] }
+    end
+
+    private
+
+    attr_reader :stations
+  end
+end
