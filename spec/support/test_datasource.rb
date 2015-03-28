45626b925338228b459aff4fab6584e6adf11b6d
Rob Murray <robmurray17@gmail.com>
2015-03-28 17:55:38 +0000

    Move away from using RSpec double away from RSpec
    Whilst this class is only used in specs its not great practice and actually broke somehow in RSpec 3.2
    
    Simpler to just use OpenStruct

--- spec/support/test_datasource.rb
+++ spec/support/test_datasource.rb
@@ -1,10 +1,7 @@
-require 'spec_helper'
-require 'rspec/mocks/standalone'
-
 class TestDatasource
   def fetch
     {
-      1 => double('station',
+      1 => TestStation.new(
                   id: 1,
                   name: 'test-station-1',
                   docks_total: 30,
@@ -12,7 +9,7 @@ class TestDatasource
                   lat: 51.53005939,
                   long: -0.120973687
       ),
-      2 => double('station',
+      2 => TestStation.new(
                   id: 2,
                   name: 'test-station-2',
                   docks_total: 30,
@@ -22,4 +19,6 @@ class TestDatasource
       )
     }
   end
+
+  TestStation = OpenStruct
 end
