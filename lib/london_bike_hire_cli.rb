8896ac35355ce6d96a7a9df31292a2ecc2e02231
Rob Murray <robmurray17@gmail.com>
2015-03-31 21:07:54 +0100

    Revert "Tidy up the require statements"
    This reverts commit bc0c0fe1bddb08fc3e0dc5138c05ae6d1b7fed37.

--- lib/london_bike_hire_cli.rb
+++ lib/london_bike_hire_cli.rb
@@ -1,4 +1,19 @@
-Dir[File.dirname(__FILE__) + "/london_bike_hire_cli/**/*.rb"].each { |file| require file }
+require_relative 'london_bike_hire_cli/application'
+require_relative 'london_bike_hire_cli/basic_renderer'
+require_relative 'london_bike_hire_cli/controller'
+require_relative 'london_bike_hire_cli/feed_parser'
+require_relative 'london_bike_hire_cli/geocoding_adapter'
+require_relative 'london_bike_hire_cli/query_response'
+require_relative 'london_bike_hire_cli/station'
+require_relative 'london_bike_hire_cli/repository/repo'
+require_relative 'london_bike_hire_cli/repository/in_memory_store_adapter'
+require_relative 'london_bike_hire_cli/repository/station_store'
+require_relative 'london_bike_hire_cli/repository/station_repo'
+require_relative 'london_bike_hire_cli/repository/spatial_search_adapter'
+require_relative 'london_bike_hire_cli/queries/stations_by_name'
+require_relative 'london_bike_hire_cli/queries/stations_near'
+require_relative 'london_bike_hire_cli/station_not_found_error'
+require_relative 'london_bike_hire_cli/version'
 
 module LondonBikeHireCli
   DEFAULT_SEARCH_LIMIT = 5
