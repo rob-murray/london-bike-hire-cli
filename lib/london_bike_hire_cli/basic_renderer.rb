7d6e1f00e0f30955c342988b296d87572cdf030f
Rob Murray <robmurray17@gmail.com>
2015-03-31 09:22:07 +0100

    Add basic colouring to terminal using ansi colours
    Think this should be good on osx and linux; screw windows :|

--- lib/london_bike_hire_cli/basic_renderer.rb
+++ lib/london_bike_hire_cli/basic_renderer.rb
@@ -5,22 +5,22 @@ module LondonBikeHireCli
     end
 
     def render(context)
-      output.print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"
-      output.print "Feed updated: #{context.first.display_feed_time}\n"
+      print_line colourize(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", :yellow)
+      print_line "Feed updated: #{colourize(context.first.display_feed_time, :red)}"
 
       context.each do |station|
         render_station(station)
       end
 
-      output.print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"
+      print_line colourize(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", :yellow)
     end
 
     def render_error(context)
-      output.print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"
-      output.print "Ooops, something went wrong with the query.\n"
-      output.print "The query #{context[:query].inspect}\n"
-      output.print "resulted in error: #{context[:error]}\n"
-      output.print "Try again?\n"
+      print_line colourize(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", :yellow)
+      print_line colourize("Ooops, something went wrong with the query.", :red)
+      print_line colourize("The query #{context[:query].inspect}", :cyan)
+      print_line colourize("resulted in error: #{context[:error]}", :cyan)
+      print_line colourize("Try again?", :yellow)
     end
 
     private
@@ -28,11 +28,34 @@ module LondonBikeHireCli
     attr_reader :output
 
     def render_station(station)
-      output.print ">>> Dock\n"
+      print_line "%s %s" % [colourize('>>>>', :yellow), colourize('Bike station', :blue)]
       station.each_pair.each do |attr_name, attr_value|
-        output.print "#{attr_name.capitalize}: #{attr_value}\n"
+        print_line "#{attr_name.capitalize}: #{colourize(attr_value, :cyan)}"
       end
-      output.print "Link to map: #{station.map_link}\n"
+      print_line "Link to map: #{colourize(station.map_link, :pink)}"
     end
+
+    def print_line(text, line_ending = "\n")
+      output.print "%s %s" % [text, line_ending]
+    end
+
+    def colourize(text, color_name = :default)
+      colour_code = colour_code(color_name)
+      "\e[#{colour_code}m#{text}\e[0m"
+    end
+
+    def colour_code(name)
+      COLOUR_MAP[name]
+    end
+
+    COLOUR_MAP = {
+      default: 39,
+      red: 31,
+      green: 32,
+      yellow: 33,
+      blue: 34,
+      pink: 35,
+      cyan: 36
+    }.freeze
   end
 end
